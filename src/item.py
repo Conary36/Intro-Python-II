4815798878aa4cedb1f8e3eb591b4f7081bf0f48
Conary36 <cbeckford144@gmail.com>
2020-09-10 00:44:10 -0400

    work in progress...

---
+++ src/item.py
@@ -0,0 +1,11 @@
+
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def on_take(self):
+        print(f'Recieved {self.name}.')
+
+    def on_drop(self):
+        print(f'Dropped {self.name}.')
--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        self.holster = []
+
+    def __str__(self):
+        return '\nName:{self.name}\nLocation:{self.current_room}\n'.format(self=self)
+
+    def collectItem(self, item_name):
+        y = [v ]
